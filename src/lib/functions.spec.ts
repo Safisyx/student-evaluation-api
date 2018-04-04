1764d16a510cae36fbcbd0615448787781fb7cb4
Safisyx <safisyx@gmail.com>
2018-04-04 21:00:01 +0200

    Add functions to allow us to compute the percentage of a color(last color for each student) in a batch

--- src/lib/functions.spec.ts
+++ src/lib/functions.spec.ts
@@ -1,6 +1,6 @@
 import 'jest'
-import {sortEval} from './functions'
-import {Evaluation} from '../students/entities'
+import {sortEval, getColorPercentage, getLastColors} from './functions'
+
 describe('sortEval', () => {
   test('Exchange if the first element happend last', () => {
 
@@ -31,5 +31,73 @@ describe('sortEval', () => {
    ]
    expect(sortEval(evaluations)).toEqual(evaluations);
 
+  })
+})
+
+describe('getLastColors', () => {
+  test('Get last colors according to date of evaluation', () => {
+    const students = [{
+        id:1,
+        name:'test1',
+        evaluations: [{
+            id: 2,
+            date: '2018-03-02',
+            code: 'red'
+          },{
+            id: 3,
+            date: '2018-01-01',
+            code: 'green'
+          }
+       ]
+     },{
+       id:2,
+       name:'test2',
+       evaluations: [{
+           id: 4,
+           date: '2018-03-02',
+           code: 'red'
+         },{
+           id: 5,
+           date: '2018-01-01',
+           code: 'green'
+         },{
+           id:6,
+           date: '2018-01-03',
+           code: 'yellow'
+         }
+      ]
+    },{
+      id:3,
+      name:'test3',
+      evaluations: [{
+          id: 10,
+          date: '2018-03-02',
+          code: 'red'
+        },{
+          id: 9,
+          date: '2018-05-01',
+          code: 'green'
+        }
+     ]
+    }
+    ]
+
+  expect(getLastColors(students)).toEqual(['red','red','green'])
+
+  })
+})
+
+
+describe('getColorPercentage', () => {
+  test('Get 50 for "red" if half of them are red', () => {
+    const colors =['red','red','yellow','green']
+
+  expect(getColorPercentage(colors,'red')).toBe(50)
+  })
+  
+  test('Get around 33.33 for "green" if 1/3 of them are green', () => {
+    const colors =['red','red','yellow','green','red','green']
+
+  expect(getColorPercentage(colors,'green')).toBe(33.33)
   })
 })
--- src/lib/functions.ts
+++ src/lib/functions.ts
@@ -6,3 +6,20 @@ export const sortEval = (evaluations) => {
     return -1
   })
 }
+
+export const getLastColors = (students) => {
+  const colors = students.map(student => {
+      const ev = sortEval(student.evaluations)
+      if (ev.length>0)
+        return ev[ev.length-1].code
+      return '0'
+    })
+    .filter(e=>(e!=='0'))
+  return colors
+}
+
+export const getColorPercentage = (colors, color:string) => {
+  const p = colors.filter(c=>c===color).length*100
+         /colors.length
+  return Math.round(p*100)/100
+}
